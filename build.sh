b573b8b1a731a643bb73cceb0a45e731445e53c1
yicll <yicllscy@139.com>
2018-12-26 18:23:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PNAME=wafer-session-server
-VERSION=1.1.0
+VERSION=1.0.0
 SCRATCH_DIR=$PNAME-$VERSION
 
 CURRDIR=`dirname "$0"`
@@ -9,7 +9,8 @@ BASEDIR=`cd "$CURRDIR"; pwd`
 export GOPATH=$BASEDIR
 export GOBIN=$BASEDIR/bin
 
-GOROOT=/usr/local/go-1.9.2
+#GOROOT=/usr/local/go-1.9.2
+GOROOT=/usr/local/go
 
 $GOROOT/bin/go clean $PNAME
 $GOROOT/bin/go install -gcflags "-N -l" $PNAME
@@ -21,7 +22,7 @@ cp -r ./bin/$PNAME src/$PNAME/conf src/$PNAME/control.sh target/$SCRATCH_DIR
 
 cd target
 
-#tar czf $SCRATCH_DIR.tar.gz $SCRATCH_DIR
-fpm -s dir -t rpm -n $PNAME -v $VERSION --iteration 1.el6 --epoch=`date +%s` --rpm-defattrfile=0755 --prefix=/usr/local/domob/prog.d $SCRATCH_DIR
+tar czf $SCRATCH_DIR.tar.gz $SCRATCH_DIR
+#fpm -s dir -t rpm -n $PNAME -v $VERSION --iteration 1.el6 --epoch=`date +%s` --rpm-defattrfile=0755 --prefix=/usr/local/domob/prog.d $SCRATCH_DIR
 
 rm -rf $SCRATCH_DIR
